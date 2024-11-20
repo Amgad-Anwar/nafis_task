da8c1b59f6f7056171ebedb156b85fdc7efd24e7
Amgad-Anwar <amgadanwar01277112438@gmail.com>
2024-11-20 03:30:31 +0200

    postman collection

--- database/migrations/2024_11_18_192243_create_task_user_table.php
+++ database/migrations/2024_11_18_192243_create_task_user_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('task_user', function (Blueprint $table) {
             $table->id();
-            
+
             $table->foreignId('task_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->timestamp('notified_at')->nullable()->default(null);            $table->timestamps();
+            $table->timestamp('notified_at')->nullable()->default(null);
 
             $table->timestamps();
         });
